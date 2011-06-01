1497e140e25bd0f4f90589799e478f87e0917496
bdrhoa <bdrhoa@gmail.com>
2011-06-01 18:49:00 +0000

    Initial import of plugin source code for the release of version 0.1
    git-svn-id: http://svn.codehaus.org/grails-plugins/grails-grails-ant/trunk@71744 832c1c66-4827-0410-8465-ccb17913cd09

---
+++ grails-app/services/org/grails/plugins/grailsant/AntUtilsService.groovy
@@ -0,0 +1,63 @@
+package org.grails.plugins.grailsant
+
+/*
+ * Reference: http://preferisco.blogspot.com/2010/06/using-goovy-antbuilder-to-zip-unzip.html
+ * Reference: http://groovy.codehaus.org/Using+Ant+from+Groovy
+ * Reference: http://groovy-almanac.org/creating-a-zipfile-with-antbuilder/
+ * Reference: http://jira.grails.org/browse/GRAILS-5675
+ * Reference: http://ant.apache.org/manual/
+ */
+
+class AntUtilsService {
+
+	static transactional = false
+
+	def unzip(String zipFile,  String destDir) {
+
+		return unzip( zipFile,   destDir, "", false)
+	}
+
+	def unzip(String zipFile,  String destDir, String mapperType) {
+
+		return unzip( zipFile,   destDir, mapperType, false)
+	}
+
+	def unzip(String zipFile,  String destDir, Boolean overwrite) {
+
+		return unzip( zipFile,   destDir, "", false)
+	}
+
+	def unzip(String zipFile,  String destDir, String mapperType, Boolean overwrite) {
+
+		def ant = new AntBuilder();   // create an antbuilder
+		if (mapperType) {
+			ant.unzip(  src: zipFile, dest:destDir,  overwrite:overwrite) {mapper(type:mapperType)}
+		} else {
+			ant.unzip(  src: zipFile, dest:destDir,  overwrite:overwrite)
+		}
+
+	}
+	
+	def zip(String destfile,String basedir) {
+		return zip(destfile,basedir,"**/*.*", "")
+	}
+	
+
+	
+	def zip(String destfile,String basedir,String includes) {
+		return zip(destfile,basedir,includes, "")	
+	}
+
+	
+	def zip(String destfile,String basedir,String includes,String excludes) {
+		
+		def ant = new AntBuilder();   // create an antbuilder
+	
+		ant.zip(destfile: destfile,
+				basedir:  basedir,
+				includes: includes,
+				excludes: excludes)
+					
+	}
+	
+}
---
+++ test/unit/org/grails/plugins/grailsant/AntUtilsServiceTests.groovy
@@ -0,0 +1,17 @@
+package org.grails.plugins.grails-ant
+
+import grails.test.*
+
+class AntUtilsServiceTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
