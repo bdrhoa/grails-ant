a10cab86ebab44eaf8fc462844cab1ec41b4cead
bdrhoa <bdrhoa@gmail.com>
2011-08-04 19:24:57 +0000

    Support controller reloading (GPANT-1).
    git-svn-id: http://svn.codehaus.org/grails-plugins/grails-grails-ant/trunk@73084 832c1c66-4827-0410-8465-ccb17913cd09

--- GrailsAntGrailsPlugin.groovy
+++ GrailsAntGrailsPlugin.groovy
@@ -1,53 +1,75 @@
 class GrailsAntGrailsPlugin {
-	// the plugin version
-	def version = "0.1.2"
-	// the version or versions of Grails the plugin is designed for
-	def grailsVersion = "1.2.3 > *"
-	// the other plugins this plugin depends on
-	def dependsOn = [:]
-	// resources that are excluded from plugin packaging
-	def pluginExcludes = [
+    // the plugin version
+    def version = "0.1.3"
+    // the version or versions of Grails the plugin is designed for
+    def grailsVersion = "1.2.3 > *"
+    // the other plugins this plugin depends on
+    def dependsOn = [:]
+    // resources that are excluded from plugin packaging
+    def pluginExcludes = [
 		"grails-app/views/error.gsp"
-	]
+    ]
 
-	// TODO Fill in these fields
-	def author = "Brad Rhoads"
-	def authorEmail = "bdrhoa@gmail.com"
-	def title = "Grails Ant Plugin"
-	def description = '''
+    // TODO Fill in these fields
+    def author = "Brad Rhoads"
+    def authorEmail = "bdrhoa@gmail.com"
+    def title = "Grails Ant Plugin"
+    def description = '''
 Provides ant to Grails applications
 '''
 
-	// URL to the plugin's documentation
-	def documentation = "http://grails.org/plugin/grails-ant"
+    // URL to the plugin's documentation
+    def documentation = "http://grails.org/plugin/grails-ant"
+    
+    def observe = [ 'controllers' ]
 
-	def doWithWebDescriptor = { xml ->
-		// TODO Implement additions to web.xml (optional), this event occurs before
-	}
+    def doWithWebDescriptor = { xml ->
+        // TODO Implement additions to web.xml (optional), this event occurs before
+    }
 
-	def doWithSpring = {
-		// TODO Implement runtime spring config (optional)
-	}
+    def doWithSpring = {
+        // TODO Implement runtime spring config (optional)
+    }
+        
+    def addAnt(clz,log) {
+                
+        def antBuilder = new AntBuilder()
+        clz.metaClass.getAnt = { antBuilder }
+            
+    }
+        
+    def doWithDynamicMethods = { ctx ->
+        
+        application.allClasses.each { clz ->
+            addAnt(clz,log)
+        }
+    }
 
-	def doWithDynamicMethods = { ctx ->
-		application.allClasses.each { clz ->
-			def antBuilder = new AntBuilder()
-			clz.metaClass.getAnt = { antBuilder }
-		}
-	}
+    def doWithApplicationContext = { applicationContext ->
+        // TODO Implement post initialization spring config (optional)
+    }
 
-	def doWithApplicationContext = { applicationContext ->
-		// TODO Implement post initialization spring config (optional)
-	}
+    def onChange = { event ->
+        // TODO Implement code that is executed when any artefact that this plugin is
+        // watching is modified and reloaded. The event contains: event.source,
+        // event.application, event.manager, event.ctx, and event.plugin.
+                
+        if (application.isControllerClass(event.source)) {
+            def controllerClass = application.getControllerClass(event.source?.name)
 
-	def onChange = { event ->
-		// TODO Implement code that is executed when any artefact that this plugin is
-		// watching is modified and reloaded. The event contains: event.source,
-		// event.application, event.manager, event.ctx, and event.plugin.
-	}
+            // If no GrailsClass can be found, i.e. 'controllerClass'
+            // is null, then this is a new controller.
+            if (controllerClass == null) {
+                controllerClass = application.addArtefact(ControllerArtefactHandler.TYPE, event.source)
+            }
 
-	def onConfigChange = { event ->
-		// TODO Implement code that is executed when the project configuration changes.
-		// The event is the same as for 'onChange'.
-	}
+            log.info "Add ant to ${controllerClass.shortName}"
+            addAnt(controllerClass, log)
+        }
+    }
+
+    def onConfigChange = { event ->
+        // TODO Implement code that is executed when the project configuration changes.
+        // The event is the same as for 'onChange'.
+    }
 }
